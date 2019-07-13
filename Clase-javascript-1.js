c0aa0724c09432de794de1c2c0dd3ab944651610
fmontalenti <flora_montalenti@hotmail.com>
2019-07-13 09:37:06 -0300

    first commit

---
+++ Clase-javascript-1.js
@@ -0,0 +1,17 @@
+// se puede hacer una variable en un solo renglón con coma:
+// var nombre, apellido, edad
+// declarar y inicializar en una misma linea:
+// var nombre = "tu vieja"
+
+var nombre;
+var apellido;
+var edad;
+var fechaDeNacimiento;
+var direccion;
+
+var miVariable;
+nombre = 'Flora';
+apellido = 'Montalenti';
+edad = 30;
+fechaDeNacimiento = '22/04/1989';
+direccion = 'en los pasillos de la villa';
